89d5450489191d2d9285fa3f50f821ac1af7b905
Rik <brik@student.ethz.ch>
2018-08-08 17:46:44 +0200

    Rename and build combinatorics.

---
+++ include/mav_coverage_graph_solvers/combinatorics.h
@@ -0,0 +1,31 @@
+#ifndef MAV_COVERAGE_GRAPH_SOLVERS_COMBINATORICS_H_
+#define MAV_COVERAGE_GRAPH_SOLVERS_COMBINATORICS_H_
+
+#include <cstddef>
+#include <set>
+#include <vector>
+
+namespace mav_coverage_planner {
+
+// Calculate the factorial
+inline int factorial(int n) {
+  return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
+}
+
+// Calculate the binomial coefficient "n choose k".
+inline int nChooseK(int n, int k) {
+  return factorial(n) / (factorial(k) * factorial(n - k));
+}
+
+// Pick all of the k-element combinations from an ordered set.
+void getAllCombinationsOfKElementsFromN(
+    const std::vector<size_t>& sorted_elements, int k,
+    std::vector<std::set<size_t>>* result);
+// The recursive call.
+void getAllCombinationsOfKElementsFromN(
+    const std::vector<size_t>& sorted_elements, int k, int start_pos,
+    std::vector<size_t>* combination, std::vector<std::set<size_t>>* result);
+
+}  // namespace mav_coverage_planner
+
+#endif /* MAV_COVERAGE_GRAPH_SOLVERS_COMBINATORICS_H_ */
