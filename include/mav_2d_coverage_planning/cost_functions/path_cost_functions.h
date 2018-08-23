c15a3a216d4b97ebca9791ec16f23ed02792394e
Rik <brik@student.ethz.ch>
2018-08-23 23:59:54 +0200

    Port velocity ramp cost function.

--- include/mav_2d_coverage_planning/cost_functions/path_cost_functions.h
+++ include/mav_2d_coverage_planning/cost_functions/path_cost_functions.h
@@ -1,8 +1,8 @@
 #ifndef MAV_2D_COVERAGE_PLANNING_COST_FUNCTIONS_PATH_COST_FUNCTIONS_H_
 #define MAV_2D_COVERAGE_PLANNING_COST_FUNCTIONS_PATH_COST_FUNCTIONS_H_
 
-#include <vector>
 #include <functional>
+#include <vector>
 
 #include <mav_coverage_planning_comm/cgal_definitions.h>
 
@@ -16,6 +16,16 @@ double computeEuclideanPathCost(const std::vector<Point_2>& path);
 
 // Given two waypoints, compute its euclidean distance.
 double computeEuclideanSegmentCost(const Point_2& from, const Point_2& to);
+
+// Given a vector of waypoints, compute its time assuming rest-to-rest
+// trapazoidal velocity profiles.
+double computeVelocityRampPathCost(const std::vector<Point_2>& path,
+                                   double v_max, double a_max);
+
+// Given two waypoints, compute its rest-to-rest time assuming trapazoidal
+// velocity profiles.
+double computeVelocityRampSegmentCost(const Point_2& from, const Point_2& to,
+                                      double v_max, double a_max);
 
 }  // namespace mav_coverage_planning
 
---
+++ src/cost_functions/path_cost_functions.cc
@@ -0,0 +1,45 @@
+#include "mav_2d_coverage_planning/cost_functions/path_cost_functions.h"
+
+#include <cmath>
+
+namespace mav_coverage_planning {
+
+double computeEuclideanPathCost(const std::vector<Point_2>& path) {
+  double distance = 0.0;
+  for (size_t i = 0; i < path.size() - 1; i++)
+    distance += computeEuclideanSegmentCost(path[i + 1], path[i]);
+
+  return distance;
+}
+
+double computeEuclideanSegmentCost(const Point_2& from, const Point_2& to) {
+  return std::sqrt(CGAL::to_double(Segment_2(from, to).squared_length()));
+}
+
+double computeVelocityRampPathCost(const std::vector<Point_2>& path,
+                                   double v_max, double a_max) {
+  double t = 0.0;
+  for (size_t i = 0; i < path.size() - 1; ++i) {
+    t += computeVelocityRampSegmentCost(path[i + 1], path[i], v_max, a_max);
+  }
+  return t;
+}
+
+double computeVelocityRampSegmentCost(const Point_2& from, const Point_2& to,
+                                      double v_max, double a_max) {
+  const double distance = computeEuclideanSegmentCost(from, to);
+  // Time to accelerate or decelerate to or from maximum velocity:
+  const double acc_time = v_max / a_max;
+  // Distance covered during complete acceleration or decelerate:
+  const double acc_distance = 0.5 * v_max * acc_time;
+  // Compute total segment time:
+  if (distance < 2.0 * acc_distance) {
+    // Case 1: Distance too small to accelerate to maximum velocity.
+    return 2.0 * std::sqrt(distance / a_max);
+  } else {
+    // Case 2: Distance long enough to accelerate to maximum velocity.
+    return 2.0 * acc_time + (distance - 2.0 * acc_distance) / v_max;
+  }
+}
+
+}  // namespace mav_coverage_planning
