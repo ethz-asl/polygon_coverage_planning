18bea94fda3c5c92870ac0f6190a17090dbb8a76
Rik Baehnemann <brik@ethz.ch>
2021-01-11 20:31:17 +0100

    Update prepare-jenkins-slave.sh

--- install/prepare-jenkins-slave.sh
+++ install/prepare-jenkins-slave.sh
@@ -4,12 +4,14 @@ ROS_VERSION=`rosversion -d`
 echo "ROS version: ${ROS_VERSION}"
 
 # Build dependencies.
-sudo apt-get install -y python-wstool python-catkin-tools
+sudo apt install -y python-wstool python-catkin-tools
 
 # Package dependencies.
 echo "Installing MAV_COMM dependencies."
-sudo apt-get install -y ros-${ROS_VERSION}-mav-msgs ros-${ROS_VERSION}-mav-planning-msgs
+sudo apt install -y ros-${ROS_VERSION}-mav-msgs ros-${ROS_VERSION}-mav-planning-msgs
 echo "Installing CGAL dependencies."
-sudo apt-get install -y libgmp-dev libmpfr-dev
+sudo apt install -y libgmp-dev libmpfr-dev
 echo "Installing MONO dependencies."
-sudo apt-get install -y mono-devel
+sudo apt install -y mono-devel
+echo "Installig GLOG dependencices."
+sudo apt install -y libgoogle-glog
