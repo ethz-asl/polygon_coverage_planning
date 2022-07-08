ed89e881fe6935f166eb5466177801b7009848f5
Rik Bähnemann <brik@ethz.ch>
2022-07-08 15:43:18 +0200

    Simplify installation as only noetic is supported from now on.

--- install/prepare-jenkins-slave.sh
+++ install/prepare-jenkins-slave.sh
@@ -4,17 +4,12 @@ ROS_VERSION=`rosversion -d`
 echo "ROS version: ${ROS_VERSION}"
 
 # Build dependencies.
-if [[ $ROS_VERSION = 'melodic' ]]
-then
-  sudo apt-get install -y python-wstool python-catkin-tools
-else
-  sudo apt-get install -y python3-wstool python3-catkin-tools
-fi
+sudo apt install -y python3-wstool python3-catkin-tools
 
 # Package dependencies.
 echo "Installing CGAL dependencies."
-sudo apt-get install -y libcgal-dev
+sudo apt install -y libcgal-dev
 echo "Installing MONO dependencies."
-sudo apt-get install -y mono-devel
+sudo apt install -y mono-devel
 echo "Installig GLOG dependencices."
-sudo apt-get install -y libgoogle-glog-dev
+sudo apt install -y libgoogle-glog-dev
