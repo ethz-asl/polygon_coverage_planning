67cfa45a35162afaf0861ea1308fb7a51e990448
Rik Bähnemann <brik@ethz.ch>
2022-07-08 12:40:43 +0200

    Revert to apt-get.

--- install/prepare-jenkins-slave.sh
+++ install/prepare-jenkins-slave.sh
@@ -6,15 +6,15 @@ echo "ROS version: ${ROS_VERSION}"
 # Build dependencies.
 if [[ $ROS_VERSION = 'melodic' ]]
 then
-  sudo apt install -y python-wstool python-catkin-tools
+  sudo apt-get install -y python-wstool python-catkin-tools
 else
-  sudo apt install -y python3-wstool python3-catkin-tools
+  sudo apt-get install -y python3-wstool python3-catkin-tools
 fi
 
 # Package dependencies.
 echo "Installing CGAL dependencies."
-sudo apt install -y libcgal-dev
+sudo apt-get install -y libcgal-dev
 echo "Installing MONO dependencies."
-sudo apt install -y mono-devel
+sudo apt-get install -y mono-devel
 echo "Installig GLOG dependencices."
-sudo apt install -y libgoogle-glog-dev
+sudo apt-get install -y libgoogle-glog-dev
